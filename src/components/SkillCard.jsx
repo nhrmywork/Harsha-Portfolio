813995a2bdc9c1ef05d202a2552b68f09ff4b926
Harsha <nhr.mywork@gmail.com>
2025-05-23 15:03:43 +0530

    Modified skill card accordingly, and fetched data from API sheet

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,12 +1,12 @@
 // src/components/SkillCard.jsx
-const SkillCard = ({ Icon, name }) => {
-    return (
-      <div className="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800 p-4 rounded-xl shadow hover:scale-105 transition-transform">
-        <Icon className="text-4xl text-blue-600 dark:text-blue-400 mb-2" />
-        <span className="font-medium text-sm">{name}</span>
-      </div>
-    );
-  };
-  
-  export default SkillCard;
-  
+const SkillCard = ({ Icon, name, description }) => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800 p-4 rounded-xl shadow hover:scale-105 transition-transform">
+      {/* <Icon className="text-4xl text-blue-600 dark:text-blue-400 mb-2" /> */}
+      <span className="font-medium text-sm">{name}</span>
+      <span className="text-gray-600 dark:text-gray-400 text-sm">{description}</span>
+    </div>
+  );
+};
+
+export default SkillCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { usePortfolio } from '../components/PortfolioContext'
+import { usePortfolio } from '../context/PortfolioContext'
 
 const Home = () => {
     const { portfolioData, loading, error } = usePortfolio();
